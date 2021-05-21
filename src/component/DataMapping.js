0369d0fcf5b0ecd3eaddaf3c7373881e278c6b6d
KyumMulani7 <kyummulani7@gmail.com>
2021-05-22 01:13:49 +0530

    Responsive

--- src/component/DataMapping.js
+++ src/component/DataMapping.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 function DataMapping(props) {
   // useEffect(() => {
   //   console.log(props.vaccine);
@@ -16,48 +16,66 @@ function DataMapping(props) {
                 (sessionData) =>
                   sessionData.vaccine === props.vaccine &&
                   sessionData.available_capacity > 0 && (
-                    <div
-                      key={sessionData.session_id}
-                      className="card-data-body"
-                    >
+                    <div key={sessionData.session_id}>
                       {sessionData.min_age_limit === props.maxage ? (
                         <>
-                          <span>
-                            <b style={{ color: '#872320' }}>Vaccine Name :</b>
-                            <span
-                              style={{ color: '#0a7022', fontWeight: 'bold' }}
-                            >
-                              {' '}
-                              {sessionData.vaccine}
-                            </span>
-                          </span>
-                          <span>
-                            <b style={{ color: 'orange' }}>Dose Left :</b>
-                            <span
-                              style={{ color: 'green', fontWeight: 'bold' }}
-                            >
-                              {' '}
-                              {sessionData.available_capacity}
-                            </span>
-                          </span>
-                          <span>
-                            <b style={{ color: 'blue' }}>Vaccine Date :</b>
-                            <span
-                              style={{ color: 'purple', fontWeight: 'bold' }}
-                            >
-                              {' '}
-                              {sessionData.date}
-                            </span>
-                          </span>
-                          <span>
-                            <b style={{ color: 'grey' }}>Age Limit :</b>
-                            <span
-                              style={{ color: '#876c20', fontWeight: 'bold' }}
-                            >
-                              {' '}
-                              {sessionData.min_age_limit}
-                            </span>
-                          </span>
+                          <Row>
+                            <Col xl={3} md={4} sm={6}>
+                              <span>
+                                <b style={{ color: '#872320' }}>
+                                  Vaccine Name :
+                                </b>
+                                <span
+                                  style={{
+                                    color: '#0a7022',
+                                    fontWeight: 'bold',
+                                  }}
+                                >
+                                  {' '}
+                                  {sessionData.vaccine}
+                                </span>
+                              </span>
+                            </Col>
+                            <Col xl={3} md={4} sm={6}>
+                              <span>
+                                <b style={{ color: 'orange' }}>Dose Left :</b>
+                                <span
+                                  style={{ color: 'green', fontWeight: 'bold' }}
+                                >
+                                  {' '}
+                                  {sessionData.available_capacity}
+                                </span>
+                              </span>
+                            </Col>
+                            <Col xl={3} md={4} sm={6}>
+                              <span>
+                                <b style={{ color: 'blue' }}>Vaccine Date :</b>
+                                <span
+                                  style={{
+                                    color: 'purple',
+                                    fontWeight: 'bold',
+                                  }}
+                                >
+                                  {' '}
+                                  {sessionData.date}
+                                </span>
+                              </span>
+                            </Col>
+                            <Col xl={3} md={4} sm={6}>
+                              <span>
+                                <b style={{ color: 'grey' }}>Age Limit :</b>
+                                <span
+                                  style={{
+                                    color: '#876c20',
+                                    fontWeight: 'bold',
+                                  }}
+                                >
+                                  {' '}
+                                  {sessionData.min_age_limit}
+                                </span>
+                              </span>
+                            </Col>
+                          </Row>
                         </>
                       ) : (
                         <p style={{ color: '#0a3370', fontWeight: 'bold' }}>
